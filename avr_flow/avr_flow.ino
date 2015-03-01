5e78c1fa23c19d0947716883cc9825b921bb8710
Marcin Klimek <github@marcinklimek.pl>
2015-03-01 19:41:13 +0100

    inactivity timeout

--- avr_flow/avr_flow.ino
+++ avr_flow/avr_flow.ino
@@ -15,17 +15,33 @@
 #define STOP_PIN   6
 #define PUMP_PIN   7
 
+#define LED_PIN    8
+#define LED_BLINK_OFF          0
+#define LED_BLINK_SUPER_FAST 100
+#define LED_BLINK_FAST       200
+#define LED_BLINK_NORMAL     500
+
+typedef void (*voidFuncPtr)(void);
+
 uint8_t price_coin_1 = 100;
 uint8_t price_coin_2 = 200;
 uint8_t price_coin_3 = 500;
 
-typedef void (*voidFuncPtr)(void);
-
 volatile uint8_t  pump_state = 0;  
 volatile uint16_t flow_pulses = 0;
-volatile uint8_t  interrupt_count[COIN_LAST]={0};  
 
-unsigned long last_millis=0;
+volatile uint8_t  interrupt_count[COIN_LAST]={0};  
+unsigned long previous_millis_coin = 0;
+
+uint8_t led_state; 
+unsigned long led_blink_interval = LED_BLINK_OFF;
+
+unsigned long previous_millis_led = 0;
+unsigned long previous_millis_console= 0;
+unsigned long previous_millis_tokens= 0;
+unsigned long previous_millis_inactivity = 0;
+
+unsigned long inactivity_timeout = 1000*30;  // 15 sec
 
 void start()
 {
@@ -33,6 +49,8 @@ void start()
     {    
         flow_pulses = 0;
         pump_state = 1;
+        
+        led_blink_interval = LED_BLINK_NORMAL;
         
         digitalWrite(PUMP_PIN, HIGH);
     }        
@@ -42,8 +60,13 @@ void stop()
 {
     digitalWrite(PUMP_PIN, LOW);
     
+    led_blink_interval = LED_BLINK_OFF;
+    led_state = HIGH;
+    digitalWrite(LED_PIN, led_state);
+    
     pump_state = 0;
     flow_pulses = 0;
+    
     for(uint8_t i=0; i<COIN_LAST; i++)
         interrupt_count[i] = 0;
 }
@@ -51,11 +74,12 @@ void stop()
 void callback_flowPin()
 {
     flow_pulses++;
+    previous_millis_inactivity = millis();
 }
 
 void callback_coinPin()
 {
-    if ( abs( millis() - last_millis ) > 500)
+    if ( abs( millis() - previous_millis_coin ) > 500)
     {
         switch( PCintPort::arduinoPin )
         {
@@ -70,7 +94,8 @@ void callback_coinPin()
                 break;                            
         }            
         
-        last_millis = millis();
+        previous_millis_coin = millis();
+        previous_millis_inactivity = previous_millis_coin;
     };
 }
 
@@ -94,12 +119,13 @@ void setup_callback(uint8_t pin, voidFuncPtr callback_ptr)
 }
 
 
-void setup() {
-    Serial.begin(9600);
-    Serial.print("Dystrybutor\n");
-
+void setup() 
+{
     pinMode(PUMP_PIN, OUTPUT);
     digitalWrite(PUMP_PIN, LOW);
+
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LOW);
 
     setup_callback(FLOW_SENSOR_PIN, &callback_flowPin);
     setup_callback(COIN_1_PIN, &callback_coinPin);
@@ -107,34 +133,80 @@ void setup() {
     setup_callback(COIN_3_PIN, &callback_coinPin);
     setup_callback(STOP_PIN, &callback_stopPin);
     
+    Serial.begin(9600);
+    Serial.print("Dystrybutor\n");    
+    
     stop();
 }
 
+
 void loop()
 {
-    Serial.print( millis() ); Serial.print( " - " );
+    unsigned long current_millis = millis();
     
-    Serial.print( "C1 " ); Serial.print( interrupt_count[COIN_1] ); Serial.print( " " );
-    Serial.print( "C2 " ); Serial.print( interrupt_count[COIN_2] ); Serial.print( " " );
-    Serial.print( "C5 " ); Serial.print( interrupt_count[COIN_3] ); Serial.print( " " );
-    
-    Serial.print( "Flow " ); Serial.print( flow_pulses ); Serial.print( " " );
-    
-    int16_t current_tokens = calculate_tokens();
-    
-    if ( current_tokens > 0 )
-    {        
-        int16_t tokens_diff = current_tokens - (int16_t)flow_pulses;
+    boolean need_endl = false;
+
+    if ( current_millis - previous_millis_inactivity > inactivity_timeout )
+    {
+        previous_millis_inactivity = current_millis;
+        stop();
+    }        
+
+    if(current_millis - previous_millis_console > 1000)
+    {
+        previous_millis_console = current_millis;
         
-        if ( tokens_diff > 0 )
-            start();
+        Serial.print( millis() ); Serial.print( " - " );
+        
+        Serial.print( "C1 " ); Serial.print( interrupt_count[COIN_1] ); Serial.print( " " );
+        Serial.print( "C2 " ); Serial.print( interrupt_count[COIN_2] ); Serial.print( " " );
+        Serial.print( "C5 " ); Serial.print( interrupt_count[COIN_3] ); Serial.print( " " );
+        
+        Serial.print( "Flow " ); Serial.print( flow_pulses ); Serial.print( " " );
+
+        need_endl = true;
+    }
+    
+    // ----------------------------------------------------------------------
+    
+    if( (led_blink_interval > 0) && (current_millis - previous_millis_led) > led_blink_interval) 
+    {
+        previous_millis_led = current_millis;
+        
+        if (led_state == LOW)
+            led_state = HIGH;
         else
-            stop();
-    }            
+            led_state = LOW;
+            
+        digitalWrite(LED_PIN, led_state);
+    }        
     
-    Serial.print( "tokens " ); Serial.print( current_tokens ); Serial.print( " " ); Serial.print(current_tokens - (int16_t)flow_pulses);
+    if (current_millis - previous_millis_tokens > 100)
+    {
+        int16_t current_tokens = calculate_tokens();
+        
+        if ( current_tokens > 0 )
+        {
+            int16_t tokens_diff = current_tokens - (int16_t)flow_pulses;
+
+            if ( (float)flow_pulses/(float)current_tokens > 0.9f )
+                led_blink_interval = LED_BLINK_SUPER_FAST;
+            else if ( (float)flow_pulses/(float)current_tokens > 0.8f )
+                led_blink_interval = LED_BLINK_FAST;
+                        
+            if ( tokens_diff > 0 )
+                start();
+            else
+                stop();
+            
+            if ( need_endl  )
+            {
+               Serial.print( "tokens " ); Serial.print( current_tokens ); Serial.print( " " ); Serial.print(current_tokens - (int16_t)flow_pulses);
+            }               
+        }
+    }
     
-    
-    Serial.print( "\n" );
-    delay(500);
+    if ( need_endl )
+        Serial.print( "\n" );   
+  
 }
